de2f80de53bc330b587b7f2ef7ba1b73f7a5d72d
Markus Roth <markus.roth@zebbra.ch>
2020-04-04 10:48:53 +0200

    more work

--- app.js
+++ app.js
@@ -122,6 +122,8 @@ app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/popper.js/d
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'), { maxAge: 31557600000 }));
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
 app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/d3-parliament'), { maxAge: 31557600000 }));
+
 
 /**
  * Primary app routes.
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,67 @@
-/* eslint-env jquery, browser */
-$(document).ready(() => {
+const domain = ['open.meet.switch.ch',  'pp.paulson.ee', 'www.kuketz-meet.de', 'together.lambda-it.ch'][0];
+const options = {
+    roomName: 'VersusVirusTeam1162',
+    width: 700,
+    height: 500,
+    parentNode: document.querySelector('#meet'),
+    configOverwrite: {
+      requireDisplayName: true,
+      startWithAudioMuted: true,
+      // filmStripOnly: true
+    },
+    userInfo: { //?
+      email: 'email@jitsiexamplemail.com'
+  }
+};
+let api = new JitsiMeetExternalAPI(domain, options);
 
-  // Place JavaScript code here...
+// setTimeout(() => {
+//   api.getAvailableDevices().then(devices => {
+//     console.log("DEBUG", devices)
+//   })
+// }, 2000)
 
+$(document).ready(function() {
+
+  $(".nav-link").click(function(e){
+    const action = $(event.target).text();
+    console.log("action", action)
+    if (action === "Hangup") {
+      api.executeCommand('hangup');
+    } else if (action === "Request to Talk") {
+      api.executeCommand('avatarUrl', 'https://avatars0.githubusercontent.com/u/3671647');
+      alert('duly noted')
+    } else if (action === "Count") {
+      alert("we have " + api.getNumberOfParticipants() + " members online")
+    } else if (action === "Home") {
+      api.executeCommand('toggleFilmStrip');
+      api.executeCommand('toggleChat');
+      api.executeCommand('subject', 'Lets talk about ' + (Math.random()*1000).toFixed(0));
+    } else if (action === "Change Room") {
+      api.executeCommand('hangup');
+      alert("you are being moved....")
+      $("#meet").empty()
+      setTimeout(() => {
+        options.roomName = options.roomName + "2"
+        api = new JitsiMeetExternalAPI(domain, options);
+      }, 1000)
+    }
+  });
+
+
+  var parliament = d3.parliament();
+  console.log("parliament d3", parliament)
+  parliament.width(600).height(400).innerRadiusCoef(0.4);
+  parliament.enter.fromCenter(true).smallToBig(true);
+  parliament.exit.toCenter(false).bigToSmall(true);
+
+  /* register event listeners */
+  parliament.on("click", function(d) { alert("You clicked on a seat of " + d.party.name); });
+  parliament.on("mouseover", function(d) { console.log("mouse on " + d.party.name); });
+  parliament.on("mouseout", function(d) { console.log("mouse out of " + d.party.name); });
+
+  /* add the parliament to the page */
+  d3.json("/data/parliament.json", function(d) {
+      d3.select("svg").datum(d).call(parliament);
+  });
 });

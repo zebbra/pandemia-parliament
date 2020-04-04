042f75880b1d59f5af0e1373da962836adfb1c56
Marek Paulson <marek@paulson.ee>
2020-04-04 11:58:13 +0200

    lobby with button to session

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,9 @@ const options = {
       email: 'email@jitsiexamplemail.com'
   }
 };
-let api = new JitsiMeetExternalAPI(domain, options);
+
+// if (window.location.href)
+// 
 
 // setTimeout(() => {
 //   api.getAvailableDevices().then(devices => {
@@ -29,46 +31,58 @@ $(document).ready(function() {
     console.log(data);
     socket.emit('respond', { message: 'Hey there, server!' });
   });
-
-  $(".nav-link").click(function(e){
-    const action = $(event.target).text();
-    console.log("action", action)
-    if (action === "Hangup") {
-      api.executeCommand('hangup');
-    } else if (action === "Request to Talk") {
-      api.executeCommand('avatarUrl', 'https://avatars0.githubusercontent.com/u/3671647');
-      alert('duly noted')
-    } else if (action === "Count") {
-      alert("we have " + api.getNumberOfParticipants() + " members online")
-    } else if (action === "Home") {
-      api.executeCommand('toggleFilmStrip');
-      api.executeCommand('toggleChat');
-      api.executeCommand('subject', 'Lets talk about ' + (Math.random()*1000).toFixed(0));
-    } else if (action === "Change Room") {
-      api.executeCommand('hangup');
-      alert("you are being moved....")
-      $("#meet").empty()
-      setTimeout(() => {
-        options.roomName = options.roomName + "2"
-        api = new JitsiMeetExternalAPI(domain, options);
-      }, 1000)
-    }
+  
+  
+  $("#gotosession").click(function(){
+    console.log('gotosession')
+    window.location.href = "/session"
+    return false;
   });
 
+  if (window.location.href.indexOf("session") > -1) {
+    let api = new JitsiMeetExternalAPI(domain, options);
+    $(".nav-link").click(function(e){
+      const action = $(event.target).text();
+      console.log("action", action)
+      if (action === "Hangup") {
+        api.executeCommand('hangup');
+      } else if (action === "Request to Talk") {
+        api.executeCommand('avatarUrl', 'https://avatars0.githubusercontent.com/u/3671647');
+        alert('duly noted')
+      } else if (action === "Count") {
+        alert("we have " + api.getNumberOfParticipants() + " members online")
+      } else if (action === "Home") {
+        api.executeCommand('toggleFilmStrip');
+        api.executeCommand('toggleChat');
+        api.executeCommand('subject', 'Lets talk about ' + (Math.random()*1000).toFixed(0));
+      } else if (action === "Change Room") {
+        api.executeCommand('hangup');
+        alert("you are being moved....")
+        $("#meet").empty()
+        setTimeout(() => {
+          options.roomName = options.roomName + "2"
+          api = new JitsiMeetExternalAPI(domain, options);
+        }, 1000)
+      }
+    });
+  
+  
+    var parliament = d3.parliament();
+    console.log("parliament d3", parliament)
+    parliament.width(600).height(400).innerRadiusCoef(0.4);
+    parliament.enter.fromCenter(true).smallToBig(true);
+    parliament.exit.toCenter(false).bigToSmall(true);
+  
+    /* register event listeners */
+    parliament.on("click", function(d) { alert("You clicked on a seat of " + d.party.name); });
+    parliament.on("mouseover", function(d) { console.log("mouse on " + d.party.name); });
+    parliament.on("mouseout", function(d) { console.log("mouse out of " + d.party.name); });
+  
+    /* add the parliament to the page */
+    d3.json("/data/parliament.json", function(d) {
+        d3.select("svg").datum(d).call(parliament);
+    });
 
-  var parliament = d3.parliament();
-  console.log("parliament d3", parliament)
-  parliament.width(600).height(400).innerRadiusCoef(0.4);
-  parliament.enter.fromCenter(true).smallToBig(true);
-  parliament.exit.toCenter(false).bigToSmall(true);
+  }
 
-  /* register event listeners */
-  parliament.on("click", function(d) { alert("You clicked on a seat of " + d.party.name); });
-  parliament.on("mouseover", function(d) { console.log("mouse on " + d.party.name); });
-  parliament.on("mouseout", function(d) { console.log("mouse out of " + d.party.name); });
-
-  /* add the parliament to the page */
-  d3.json("/data/parliament.json", function(d) {
-      d3.select("svg").datum(d).call(parliament);
-  });
 });
